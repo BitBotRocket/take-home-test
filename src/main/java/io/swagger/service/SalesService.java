b3f04aae01eb1f27a286ec7d00d2ef3b421663e0
Dave Adams <adams.dave.m@gmail.com>
2024-10-09 01:41:32 -0400

    refactor SalesService, document todos and Questions

--- src/main/java/io/swagger/service/SalesService.java
+++ src/main/java/io/swagger/service/SalesService.java
@@ -33,53 +33,46 @@ public class SalesService {
         this.discounts = discounts;
     }
 
-    /*
-     * TODO: lots of good junit opportunities here
-     */
-
-    public SalesResponse processSalesRequest(SalesRequest request) {
-
-        final SalesResponse salesResponse = new SalesResponse();
-
-        /*
-         * Cross-Reference the product items in the request with the product database
-         */
-
+    private List<Product> findMatchingProductsFromRequest(List<SalesRequestProductItems> productItems) {
         List<Product> products = new ArrayList<>();
-
-        /*
-         * TODO: What happens when the product is not found in the database?
-         * What happens when the product is found but the price is 0.0?
-         * What happens when the product is found but the price is negative?
-         * What should the default response be if the cart is empty?
-         * Need to test for no applied discounts and no products in the cart
-         * Need to test for no applied discounts and products in the cart
-         */
-
-        for (SalesRequestProductItems item : request.getProductItems()) {
+        for (SalesRequestProductItems item : productItems) {
             final Product p = productDatabase.retrieve(item.getId());
             if (p != null) {
                 products.add(p);
             }
         }
+        return products;
+    }
 
-        /*
-         * Determine total of all "fixed" discounts and apply uniformly to all products
-         */
+    private float getFixedDiscountTotal(SalesRequest request) {
 
         float fixedDiscountTotal = 0.0f;
-        float percentDiscountTotal = 0.0f;
-        float uniformDiscount = 0.0f;
-
-        List<SalesResponseAppliedDiscounts> appliedDiscounts = new ArrayList<>();
-
         for (String discountCode : request.getDiscountCodes()) {
-            System.out.println("discountCode = " + discountCode);
-
-            SalesResponseAppliedDiscounts appliedDiscount = null;
-
             final Discount discount = discounts.retrieveDiscount(discountCode);
+            if (discount != null && discount.getDiscountUnit().equals(DiscountUnitEnum.FIXED)) {
+                fixedDiscountTotal += discount.getDiscount();
+            }
+        }
+        return fixedDiscountTotal;
+    }
 
+    private float getPercentDiscountTotal(SalesRequest request) {
+
+        float percentDiscountTotal = 0.0f;
+        for (String discountCode : request.getDiscountCodes()) {
+            final Discount discount = discounts.retrieveDiscount(discountCode);
+            if (discount != null && discount.getDiscountUnit().equals(DiscountUnitEnum.PERCENT)) {
+                percentDiscountTotal += discount.getDiscount();
+            }
+        }
+        return percentDiscountTotal;
+    }
+
+    private List<SalesResponseAppliedDiscounts> getAppliedDiscounts(SalesRequest request) {
+        List<SalesResponseAppliedDiscounts> appliedDiscounts = new ArrayList<>();
+        for (String discountCode : request.getDiscountCodes()) {
+            SalesResponseAppliedDiscounts appliedDiscount = null;
+            final Discount discount = discounts.retrieveDiscount(discountCode);
             if (discount != null) {
                 appliedDiscount = new SalesResponseAppliedDiscounts();
                 appliedDiscount.setCode(discountCode);
@@ -87,35 +80,14 @@ public class SalesService {
                 appliedDiscount.setDiscountUnit(discount.getDiscountUnit());
                 appliedDiscounts.add(appliedDiscount);
             }
-
-            if (discount != null && discount.getDiscountUnit().equals(DiscountUnitEnum.FIXED)) {
-                fixedDiscountTotal += discount.getDiscount();
-            } else if (discount != null && discount.getDiscountUnit().equals(DiscountUnitEnum.PERCENT)) {
-                percentDiscountTotal += discount.getDiscount();
-            } else {
-                System.out.println("Discount not found or not fixed with code = " + discountCode);
-            }
         }
+        return appliedDiscounts;
+    }
 
-        salesResponse.setAppliedDiscounts(appliedDiscounts);
-
-        if (products.size() > 0) {
-            /*
-             * TODO: What happens when the applied uniform discount > than the price of the
-             * product?
-             */
-            uniformDiscount = fixedDiscountTotal / products.size();
-        }
-
-        System.out.println("Uniform discount = " + uniformDiscount);
-        System.out.println("Total percent discount = " + percentDiscountTotal);
-
-        /*
-         * For each product apply the discounts
-         */
+    private List<SalesResponseItems> buildSaleItemsWithAppliedDiscounts(List<Product> products, SalesRequest request,
+            float uniformDiscount, float percentDiscountTotal) {
 
         List<SalesResponseItems> saleItems = new ArrayList<>();
-
 
         for (SalesRequestProductItems item : request.getProductItems()) {
 
@@ -124,35 +96,35 @@ public class SalesService {
             if (p != null) {
 
                 SalesResponseItems salesResponseItem = new SalesResponseItems();
-
                 float price = p.getPrice();
                 float quantity = item.getQuantity();
                 float discount = 0.0f;
                 float totalBeforeTax = price * quantity;
-    
+
                 if (uniformDiscount > 0.0f) {
                     discount += uniformDiscount;
                 }
-    
+
                 if (percentDiscountTotal > 0.0f) {
                     discount += price * quantity * percentDiscountTotal;
                 }
-    
+
                 totalBeforeTax -= discount;
-    
+
                 salesResponseItem.setAppliedDiscount(discount);
                 salesResponseItem.setUnitPrice(price);
                 salesResponseItem.setId(p.getId());
                 salesResponseItem.setQuantity(new BigDecimal(quantity));
                 salesResponseItem.setTotalBeforeTax(totalBeforeTax);
-    
+
                 saleItems.add(salesResponseItem);
-
             }
+
         }
+        return saleItems;
+    }
 
-        salesResponse.setItems(saleItems);
-
+    private SalesResponseSalesSummary buildSalesSummary(List<SalesResponseItems> saleItems) {
         float totalBeforeTax = 0.0f;
         for (SalesResponseItems item : saleItems) {
             totalBeforeTax += item.getTotalBeforeTax();
@@ -166,11 +138,49 @@ public class SalesService {
         salesSummary.setTaxes(totalBeforeTax * 0.13f);
         salesSummary.setTotalAfterTaxes(totalBeforeTax * 1.13f);
 
-        salesResponse.setSalesSummary(salesSummary);
+        return salesSummary;
+
+    }
+
+    /*
+     * TODO: lots of good junit opportunities here
+     */
+
+    public SalesResponse processSalesRequest(SalesRequest request) {
+
+        final SalesResponse salesResponse = new SalesResponse();
 
         /*
-         * TODO: refactor into smaller methods
+         * Cross-Reference the product items in the request with the product database
          */
+
+        List<Product> products = findMatchingProductsFromRequest(request.getProductItems());
+
+        /*
+         * Determine total of all "fixed" discounts and apply uniformly to all products
+         */
+
+        float fixedDiscountTotal = getFixedDiscountTotal(request);
+        float percentDiscountTotal = getPercentDiscountTotal(request);
+        float uniformDiscount = 0.0f;
+
+        salesResponse.setAppliedDiscounts(getAppliedDiscounts(request));
+
+        if (products.size() > 0) {
+            /*
+             * TODO: What happens when the applied uniform discount > than the price of the
+             * product?
+             */
+            uniformDiscount = fixedDiscountTotal / products.size();
+        }
+
+        final List<SalesResponseItems> saleItems = buildSaleItemsWithAppliedDiscounts(products, request,
+                uniformDiscount,
+                percentDiscountTotal);
+
+        salesResponse.setItems(saleItems);
+
+        salesResponse.setSalesSummary(buildSalesSummary(saleItems));
 
         return salesResponse;
 
